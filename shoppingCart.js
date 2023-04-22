7e17afbb15297a1ebe396123207a16bd7cf2865a
adan206 <adanweli@hotmail.com>
2023-04-22 03:00:12 -0700

    lastest

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,5 +1,10 @@
+import items from './items.json';
+import formatCurrency from './util/formatCurrency.js';
 const cartItemWrapper = document.querySelector('[data-cart-item-wrapper]');
 const cartButton = document.querySelector('[ data-cart-button]');
+const cartItemTemplate = document.querySelector('[cart-item-template]');
+const cartItemContainer = document.querySelector('[data-cart-items]');
+const IMAGE_URL_CART = 'https://dummyimage.com/210x130';
 
 let shoppingCart = [];
 
@@ -19,6 +24,30 @@ cartButton.addEventListener('click', (e) => {
 export function addToCart(id) {
   shoppingCart.push({ id: id, quantity: 1 });
   console.log(shoppingCart);
+
+  renderCart();
+}
+
+function renderCart() {
+  shoppingCart.forEach((entry) => {
+    const item = items.forEach((i) => entry.id === i.id);
+
+    const cartItem = cartItemTemplate.content.cloneNode(true);
+
+    const container = cartItem.querySelector('[data-item]');
+    container.dataset.itemId = item.id;
+
+    const name = cartItem.querySelector('[data-name]');
+    name.innerText = item.name;
+
+    const image = cartItem.querySelector('[data-image]');
+    image.src = `${IMAGE_URL_CART}/${item.imageColor}/${item.imageColor}`;
+
+    const price = cartItem.querySelector('[data-price]');
+    price.innerText = formatCurrency(item.priceCents / 100);
+
+    cartItemContainer.appendChild(crtItem);
+  });
 }
 
 // add item to cart
