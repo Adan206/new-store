ebf4a21b773bead6a2958eb3b4ffecaa99f09617
adan206 <adanweli@hotmail.com>
2023-04-22 00:34:40 -0700

    changes

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,6 +1,8 @@
 const cartItemWrapper = document.querySelector('[data-cart-item-wrapper]');
 const cartButton = document.querySelector('[ data-cart-button]');
 
+let shoppingCart = [];
+
 export function setupShoppingCart() {}
 
 // add items to cart
@@ -13,3 +15,13 @@ export function setupShoppingCart() {}
 cartButton.addEventListener('click', (e) => {
   cartItemWrapper.classList.toggle('invisible');
 });
+
+export function addToCart(id) {
+  shoppingCart.push({ id: id, quantity: 1 });
+  console.log(shoppingCart);
+}
+
+// add item to cart
+// handle click event for adding
+// handle multiple of the same item in cart
+// calculate an accute total
